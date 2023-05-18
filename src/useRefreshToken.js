b7b74cf12bbe818a392dafe864572ef1ace7177e
Ajmalka84 <109918972+Ajmalka84@users.noreply.github.com>
2023-05-18 10:12:18 +0530

    port commit

--- src/useRefreshToken.js
+++ src/useRefreshToken.js
@@ -8,7 +8,7 @@ const useRefreshToken = () => {
   const navigate = useNavigate()
   const refresh = async () => {
     await axios
-      .get("http://localhost:8000/auth/refresh", { withCredentials: true })
+      .get("http://3.83.38.239/auth/refresh", { withCredentials: true })
       .then((result) => {
         setAuth((prev) => {
           return {
